0a77783583d72f900873edd721732253b9f9ed13
dltt83 <dltt83@durham.ac.uk>
2022-08-11 19:09:18 +0100

    partly refactored code to remove unnecessary, added wheel position detection and indicator on window

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 import pyautogui
 import cv2
 import keyboard
@@ -14,15 +13,12 @@ global RIGHT_BASE
 # define booleans for control flow
 run = False
 quitLoop = False
-# initialise base position variables as constants
-LEFT_BASE = 110
-RIGHT_BASE = 42
-
 
 #define hotkey functions
 def changeRun():
     global run
     run = not run
+    print("running", run)
 # END DEF
 
 def stopLoop():
@@ -31,161 +27,90 @@ def stopLoop():
 # END DEF
 
 
-def updateDisplay(win, frame, aveXL, aveXR):
-    win.fill((0, 0, 0))
-
-    img = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
-    im_pil = Image.fromarray(img)
-
-    py_image = pygame.image.fromstring(im_pil.tobytes(), im_pil.size, im_pil.mode)
-
-    win.blit(py_image, (0,0))
-
-    pygame.draw.rect(win, (255, 0, 0), pygame.Rect(aveXL, 0, 1, 100))
-    pygame.draw.rect(win, (0, 255, 0), pygame.Rect(aveXR+200, 0, 1, 100))
-    pygame.draw.rect(win, (0, 0, 255), pygame.Rect(200, 0, 1, 100))
-    
-    pygame.display.update()
-# END DEF
-
-
-def processImage(frame, win):
-    (thresh, frameBW) = cv2.threshold(frame, 180, 255, cv2.THRESH_BINARY)
-
-
-    leftSide = frameBW[0:100, 0:200]
-    rightSide = frameBW[0:100, 201:320]
-
-    # find average x position of white pixels
-    aveXL = 0
-    whtNumL = 0
-    for y in range(len(leftSide)):
-        for x in range(len(leftSide[0])):
-            if leftSide[y][x] == 255:
-                aveXL = aveXL + x
-                whtNumL += 1
-    #END FOR
-
-    aveXR = 0
-    whtNumR = 0
-    for y in range(len(rightSide)):
-        for x in range(len(rightSide[0])):
-            if rightSide[y][x] == 255:
-                aveXR = aveXR + x
-                whtNumR += 1
-    # END FOR
-
-    if whtNumL > 0:
-        aveXL = aveXL / whtNumL
-    else:
-        aveXL = LEFT_BASE
-    # END IF
-    if whtNumR > 0:
-        aveXR = aveXR / whtNumR
-    else:
-        aveXR = RIGHT_BASE
-    # END IF
-
-    updateDisplay(win, frameBW, aveXL, aveXR)
-    return aveXL, aveXR #, frameBW
-# END DEF
-
-
-def processImageForLines(frame, win):
-    # apply gaussian blur to image
-    kernel_size = 5
-    blurFrame = cv2.GaussianBlur(frame ,(kernel_size, kernel_size),0)
-
-    # convert image to binary black and white
-    (thresh, frameBW) = cv2.threshold(blurFrame, 180, 255, cv2.THRESH_BINARY)
-
-    # use canny edge detection
-    low_threshold = 50
-    high_threshold = 150
-    edges = cv2.Canny(frameBW, low_threshold, high_threshold)
-
-    rho = 1  # distance resolution in pixels of the Hough grid
-    theta = np.pi / 180  # angular resolution in radians of the Hough grid
-    threshold = 15  # minimum number of votes (intersections in Hough grid cell)
-    min_line_length = 20  # minimum number of pixels making up a line
-    max_line_gap = 60  # maximum gap in pixels between connectable line segments
-    line_image = np.copy(frameBW) * 0  # creating a blank to draw lines on
-
-    # Run Hough on edge detected image
-    # Output "lines" is an array containing endpoints of detected line segments
-    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
-
-    for line in lines:
-        for x1,y1,x2,y2 in line:
-            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
-    
-    lines_edges = cv2.addWeighted(frameBW, 0.8, line_image, 1, 0)
-
-    
-    # update pygame window with information
-    win.fill((0, 0, 0))
-
-    # img = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
-    im_pil = Image.fromarray(lines_edges)
-
-    py_image = pygame.image.fromstring(im_pil.tobytes(), im_pil.size, im_pil.mode)
-
-    win.blit(py_image, (0,0))
-
-    # pygame.draw.rect(win, (255, 0, 0), pygame.Rect(aveXL, 0, 1, 100))
-    # pygame.draw.rect(win, (0, 255, 0), pygame.Rect(aveXR+200, 0, 1, 100))
-    # pygame.draw.rect(win, (0, 0, 255), pygame.Rect(200, 0, 1, 100))
-    
-    pygame.display.update()
-# END DEF
-
-
 # set hotkeys
 keyboard.add_hotkey("#", lambda: changeRun())
 keyboard.add_hotkey("l", lambda: stopLoop())
 
 
+def updateDisplay(frame, win, wheelPos):
+    # update pygame window with information
+    win.fill((0, 0, 0))
+
+    # img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    im_pil = Image.fromarray(frame)
+
+    py_image = pygame.image.fromstring(im_pil.tobytes(), im_pil.size, im_pil.mode)
+
+    win.blit(py_image, (0,0))
+
+    if wheelPos < 0:
+        pygame.draw.rect(win, (255, 0, 0), pygame.Rect((210+wheelPos*10), 158, wheelPos*-10, 30))
+    elif wheelPos > 0:
+        pygame.draw.rect(win, (255, 0, 0), pygame.Rect(210, 158, wheelPos*10, 30))
+    # END IF
+
+    
+    pygame.display.update()
+# END DEF
+
+
+def processImageForLines(frame, win):
+    print("processing")
+# END DEF
+
+def getWheelPosition(frame):
+    # checkpoints array from wheel positions.py
+    checkpoints = [[472, 254], [479, 262], [487, 271], [495, 279], [503, 288], [510, 296], [518, 305], [526, 314], [534, 323], [541, 331], [549, 340], [557, 348], [565, 357], [573, 365], [581, 374], [589, 383], [597, 392], [601, 404], [605, 416], [609, 428], [613, 441], [617, 453], [621, 465], [625, 477], [630, 490], [634, 502], [638, 514], [642, 526], [647, 539], [651, 551], [655, 563], [659, 575], [664, 588]]
+
+    threshhold = 20
+
+    result = []
+    for position in checkpoints:
+        point = frame[position[1]][position[0]]
+        if sum(point) / 3 > threshhold:
+            result.append(1)
+        else:
+            result.append(0)
+        # END IF
+    # END FOR
+
+    return sum(result) - 16
+# END DEF
+
+
 # main function
 def main():
+    screenshotDimensions = (700, 450, 700, 600)
+
     # start pygame display
     pygame.init()
-    win = pygame.display.set_mode((320, 100))
+    win = pygame.display.set_mode((420, 188)) # frame is 420x158 + 30 at bottom for inidcator
     pygame.display.update()
 
     # add global variables
-    global run, quitLoop, LEFT_BASE, RIGHT_BASE
+    global run, quitLoop
+
+    badFrames = 0
 
     # start loop
     while not quitLoop:
         if run:
             # get image
-            screenshot = pyautogui.screenshot(region=(750, 514, 320, 100))
+            screenshot = pyautogui.screenshot(region=screenshotDimensions)
             # convert to np and grayscale
-            frame = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2GRAY)
+            frame = np.array(screenshot)
 
-            # # ==========
-            # # process image
-            # aveXL, aveXR = processImage(frame, win)
+            # print(sum(frame[508][176]))
+            if sum(frame[508][176]) == 0:
+                badFrames += 1
+                print("badFrame", badFrames)
+            else:
+                roadFrame = frame[0:158, 0:420]
+                # processImageForLines(roadFrame, win)
+                wheelPos = getWheelPosition(frame)
 
-            # # determine action
-            # lOffset = aveXL - LEFT_BASE
-            # rOffset = aveXR - RIGHT_BASE
-
-            # netOffset = lOffset + rOffset
-            # print(aveXL, aveXR)
-            # print(netOffset)
-
-            # # do action
-            # if netOffset < -50:
-            #     keyboard.press("a")
-            # elif netOffset > 50:
-            #     keyboard.press("d")
-            # # END IF
-            # # ==========
-
-            processImageForLines(frame, win)
-        else:
-            print("not running")
+                updateDisplay(roadFrame, win, wheelPos)
+            # END IF
         # END IF
 
         for event in pygame.event.get():
@@ -193,12 +118,10 @@ def main():
                 # stops loop and closes window
                 pygame.quit()
         # END FOR
-
-        time.sleep(0.03)
-        keyboard.release("a")
-        keyboard.release("d")
-        time.sleep(0.17)
     # END WHILE
 # END DEF
 
 main()
+
+
+# =================================================================
